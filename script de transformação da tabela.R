bad44a392e8903ee865e0456e3f358834de2d553
marcelotournier <dr.tournier@gmail.com>
2017-10-19 23:42:12 -0200

    final version

--- script de transformação da tabela.R
+++ script de transformação da tabela.R
@@ -1,34 +1,34 @@
-setwd("C:/R/obeso")
-# Para o pacote tabulizer funcionar, é necessário instalar o jdk
-library(tabulizer)
-library(dplyr)
-library(reshape)
+# setwd("C:/R/obeso")
+# For the tabulizer package to work, jdk is needed
 
-# TABELA 1 - "t1.pdf"
+require(tabulizer)
+require(dplyr)
+require(reshape)
 
-#extrair tabela do pdf
+# TABLE 1 - "t1.pdf"
+
+#extract table from pdf
 out <- extract_tables("t1.pdf")
 final <- do.call(rbind, out)
 
-# PADRONIZAR NOMES DE PAÍSES PARA O WORLD BANK
+# Correct country names to match World Bank Country List (See package "WDI" for reference)
 
-# corrigir nomes dos países
 final[23,1]= "Bosnia and Herzegovina"
-final[34,1]="Central African Republic"
+final[34,1]= "Central African Republic"
 final[51,1] = "Egypt, Arab Rep."
 final[131,1] = "St. Vincent and the Grenadines"
 
 
-# tirar as duas primeiras colunas da tabela
+# Remove first rows from table
 final2 <- as.data.frame(final[-c(1:2),])
 
-# colocar os nomes no Data frame
+# col names in Data frame
 
 
 names(final2) <-c("Country","2014","2025","2014","2025")
 
 
-# limpar os dados
+# Cleaning and Tidying
 f2 <- final2[,c(1,2)]
 f3 <- final2[,c(1,3)]
 f4 <- final2[,c(1,4)]
@@ -39,7 +39,7 @@ fin3 <- melt(f3, id=1)
 fin4 <- melt(f4, id=1)
 fin5 <- melt(f5, id=1)
 
-#colocar a coluna de ano nas tabelas fin2 a fin5
+#Put Year cols in DFs fin2 a fin5
 
 fin2$Year <- 2014
 fin3$Year <- 2025
@@ -51,19 +51,17 @@ fin3$variable <- "Estimated prevalence of obesity (% of all adults)"
 fin4$variable <- "Number of adults (thousands) with obesity BMI 30+"
 fin5$variable <- "Cost (US$ Millions) to reduce number of adults with obesity by 1%"
 
-# juntar tudo em um data frame
+# join into a data frame
 
 obeso1 <- rbind(fin2,fin3,fin4,fin5)
 
-# TABELA 2 - "t2.pdf"
-#extrair tabela do pdf
+# TABLE 2 - "t2.pdf"
+#extract table from pdf
 
 out2 <- extract_tables("t2.pdf")
 bfinal <- do.call(rbind, out2)
 
-# PADRONIZAR NOMES DE PAÍSES PARA O WORLD BANK
-
-# Corrigir nomes de países na tabela
+# Correct country names to match World Bank Country List (See package "WDI" for reference)
 
 bfinal[1,1] = "Country"
 bfinal[7,1] = "Antigua and Barbuda"
@@ -71,23 +69,25 @@ bfinal[13,1] = "Bahamas, The"
 bfinal[22,1] = "Bolivia"
 bfinal[23,1] = "Bosnia and Herzegovina"
 bfinal[30,1] = "Cabo Verde"
-bfinal[34,1] = "Central African Repuplic"
+bfinal[34,1] = "Central African Republic"
 bfinal[40,1] = "Congo, Rep."
 bfinal[42,1] = "Cote d'Ivoire"
 bfinal[51,1] = "Egypt, Arab Rep."
 bfinal[60,1] = "Gambia, The"
 bfinal[87,1] = "Kyrgyz Republic"
 bfinal[88,1] = "Lao PDR"
+bfinal[95,1] = "Macedonia, FYR"
 bfinal[104,1] = "Micronesia, Fed. Sts."
 bfinal[130,1] = "St. Lucia"
 bfinal[131,1]= "St. Vincent and the Grenadines"
 bfinal[133,1]= "Sao Tome and Principe"
 bfinal[164,1]="United Arab Emirates"
 bfinal[166,1]="United States"
+bfinal[170,1] = "Vietnam"
 bfinal[171,1]="Yemen, Rep."
 bfinal[174,1] = "World"
 
-# tirar as duas primeiras colunas da tabela
+# Remove the first two rows
 
 bfinal2 <- as.data.frame(bfinal)
 
@@ -99,22 +99,22 @@ obeso2 <- melt(bfinal2,id = "Country", variable_name = "Year")
 
 obeso2[,4]="Healthcare cost of consequences of high BMI (US$ Million - purchasing power parity)"
 
-# mudar ordem das colunas para igualar ao Data frame obeso1
+# Change col order to equal  Data frame obeso1
 
 obeso2 <- obeso2[c(1,4,3,2)]
 
-# mudar nome das colunas para igualar ao Data frame obeso1
+# Change col names to equal Data frame obeso1
 
 names(obeso2) <- c("Country","variable","value","Year")
 
-# TABELA 3 - "t5.pdf"
-#extrair tabela do pdf
+# Table 3 - "t5.pdf"
+#extract pdf table
 
 out3 <- extract_tables("t5.pdf")
 
 cfinal <- rbind(out3[[1]],out3[[2]],out3[[3]],out3[[4]])
 
-# tabela possui incoerencias na ultima pagina (objeto "cfinal2"), que precisa ser corrigiga...
+# Table has incoerencies in the last page (object "cfinal2"), that needs to be corrected...
 
 cfinal2 <- rbind(out3[[5]])
 
@@ -124,13 +124,15 @@ cfinal2 <- cfinal2[,-c(2,8,11)]
 
 cfinal2[8,2:13] <- cfinal2[11,2:13]
 
+cfinal2[4,1] = "Vietnam"
+
 cfinal2[8,1]="World"
 
-# juntar pagina 5 com as demais páginas
+# Join page 5 with the others
 
 cfinalf <- rbind(cfinal, cfinal2)
 
-# PADRONIZAR NOMES DE PAÍSES PARA O WORLD BANK
+# STANDARDIZE COUNTRY NAMES TO THE WORLD BANK LIST (See Package "WDI" for reference)
 
 
 cfinalf[1,1]="Country"
@@ -156,7 +158,7 @@ cfinalf <- as.data.frame(cfinalf[2:182,])
 names(cfinalf) <- c("Country","2014","2014","2014","2014","2014","2014","2025","2025","2025","2025","2025","2025")
 
 
-# Preparar as subtabelas para o bind...
+# Prepararing data to bind...
 
 cf1 <- c("","Total Adult Population (thousands)", "Adult population - not overweight (thousands)", "Adult population - BMI 25-30 (thousands)","Adult population - BMI 30-35 (thousands)","Adult population - BMI 35-40 (thousands)","Adult population - BMI > 40 (thousands)","Total Adult Population (thousands)", "Adult population - not overweight (thousands)", "Adult population - BMI 25-30 (thousands)","Adult population - BMI 30-35 (thousands)","Adult population - BMI 35-40 (thousands)","Adult population - BMI > 40 (thousands)")
 
@@ -201,58 +203,19 @@ cf13[,4]=cf1[13]
 
 obeso5 <- rbind(cf2,cf3,cf4,cf5,cf6,cf7,cf8,cf9,cf10,cf11,cf12,cf13)
 
-# mudar ordem das colunas para igualar ao Data frame obeso1
+# change col names and order to equal Data frame obeso1
 
 obeso5 <- obeso5[c(1,4,3,2)]
 
-# mudar nome das colunas para igualar ao Data frame obeso1
+# change col names to equal Data frame obeso1
 
 names(obeso5) <- c("Country","variable","value","Year")
 
-# Juntar todas as tabelas e salvar como um arquivo CSV
+# Joining all tables
 
 obesity = rbind(obeso1,obeso2,obeso5)
 
-# Se necessário, substituir os pontos da coluna de "values" por virgulas:
-# obesity[,3]=gsub("\\.",",",obesity[,3])
-
-# encontrar strings erradas na coluna "Country"
-# which(obeso$Country == "Antigua and Barbud")
-# a=which(obeso$Country == "Antigua and Barbud")
-# obeso[a,1]="Antigua and Barbuda"
-
-# which(obeso$Country == "All Countries")
-# a=which(obeso$Country == "All Countries")
-# a=as.data.frame(a)
-# obeso[a,1]="World"
-
-# nomes certos: #################3
-# Antigua and Barbuda
-# World
-# Bahamas, The (Bahamas)
-# Bolivia (Bolivia Plurinational)
-# Bosnia and Herzegovina (Bosnia and Herzegov, Bosnia and Herzegovin)
-# Cabo Verde (Cabo Verde Republic, Cabo Verde Republic of)
-# Central African Republic (Central African Repu, Central African Republi)
-# Congo, Rep. (Congo)
-# Congo, Dem. Rep. (Congo, Dem. Rep. , DR Congo)
-# Egypt, Arab Rep. (Egypt)
-# Macedonia, FYR (FYR Macedonia, Macedonia)
-# Gambia, The (Gambia)
-# Iran, Islamic Rep. (Iran)
-# Kyrgyz Republic (Kyrgyzstan)
-# Lao PDR (Lao People's Democ)
-# Micronesia, Fed. Sts. (Micronesia (Fed), "(Micronesia (Federat")
-# St. Lucia (Saint Lucia)
-# St. Vincent and the Grenadines (St. Vincent and th, St. Vincent and the G)
-# Sao Tome and Principe (Sao Tome and Princ)
-# United Arab Emirates (United Arab Emirate)
-# United States (United States of Am, United States of Americ)
-# Venezuela, RB (Venezuela)
-# Yemen, Rep. (Yemen)
-
-
-# ajustes finais do Data Frame para exportar
+# Turn column Value as numeric
 
 obesity$value = as.numeric(as.character(obesity$value))
 
